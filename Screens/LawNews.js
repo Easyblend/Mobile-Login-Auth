13890af8798b4345932d271f4968166257b6f064
easyblend <kenzieemma072@gmail.com>
2023-07-27 00:21:13 +0000

    added a news feed page

--- Screens/LawNews.js
+++ Screens/LawNews.js
@@ -6,16 +6,38 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome } from "@expo/vector-icons";
 import NewsBox from "../component/NewsBox";
-import { ScrollView } from "react-native";
+import { ScrollView, RefreshControl } from "react-native";
+import qs from "qs";
 
-const LawNews = () => {
+const LawNews = ({ navigation }) => {
+  const query = [
+    "law",
+    "legal",
+    "lawyer",
+    "court",
+    "arrest",
+    "constitution",
+    "regulation",
+    "federal",
+    "Domestic",
+    "rape",
+    "ghana",
+    "africa",
+    "Accra",
+    "nigeria",
+    "south africa",
+  ];
+
+  const baseURL = "https://newsdata2.p.rapidapi.com/news";
+  const queryParams = {
+    language: "en",
+    q: query[Math.floor(Math.random() * query.length)],
+  };
+  const queryString = qs.stringify(queryParams);
+  const url = `${baseURL}?${queryString}`;
+
   const options = {
     method: "GET",
-    url: "https://newsdata2.p.rapidapi.com/news",
-    params: {
-      language: "en",
-      q: "law",
-    },
     headers: {
       "X-RapidAPI-Key": "9c11b549dbmshc1de7d2079a5344p1b4042jsn0dfd7e13f437",
       "X-RapidAPI-Host": "newsdata2.p.rapidapi.com",
@@ -25,127 +47,97 @@ const LawNews = () => {
   const [newState, setNewState] = useState(null);
 
   useEffect(() => {
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await fetch(options.url, options);
-    //     const data = await response.json();
-    //     setNewState(data.results);
-    //   } catch (error) {
-    //     alert(error);
-    //   }
-    // };
-
-    // fetchData();
-    setNewState(array1);
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setNewState(data.results);
+      })
+      .catch((error) => {
+        alert(error);
+      });
   }, []);
 
-  const array1 = [
-    {
-      category: ["business"],
-      content:
-        "Printable Reflective Films Market Future Outlook (2023-2029) Trends, Scope, Size, Demand, Competitors Study, by Region, Top Players...",
-      country: ["germany"],
-      keywords: ["Business, Economy, Finances, Banking & Insurance"],
-      language: "english",
-      link: "https://www.openpr.com/news/3147154/printable-reflective-films-market-future-outlook-2023-2029",
-      pubDate: "2023-07-26 20:14:46",
-      title:
-        "Printable Reflective Films Market Future Outlook (2023-2029) Trends, Scope, Size, Demand, Competitors Study, by Region, Top Players - Imperx Inc, Matrox Imaging Ltd., ADLINK Technology Inc",
-      video_url: null,
-    },
-    // Additional objects
-    {
-      category: ["technology"],
-      content:
-        "New Advancements in Artificial Intelligence: A Comprehensive Overview...",
-      country: ["united states"],
-      keywords: ["Technology, AI, Artificial Intelligence"],
-      language: "english",
-      link: "https://www.example.com/ai-advancements",
-      pubDate: "2023-07-26 15:30:00",
-      title:
-        "New Advancements in Artificial Intelligence: A Comprehensive Overview",
-      video_url: null,
-    },
-    {
-      category: ["entertainment"],
-      content: "Upcoming Movie Releases: Blockbusters to Watch Out For...",
-      country: ["united kingdom"],
-      keywords: ["Entertainment, Movies, Upcoming Releases"],
-      language: "english",
-      link: "https://www.example.com/upcoming-movies",
-      pubDate: "2023-07-26 12:00:00",
-      title: "Upcoming Movie Releases: Blockbusters to Watch Out For",
-      video_url: null,
-    },
-    {
-      category: ["health"],
-      content:
-        "Tips for a Healthy Lifestyle: Diet and Exercise Recommendations...",
-      country: ["canada"],
-      keywords: ["Health, Wellness, Diet, Exercise"],
-      language: "english",
-      link: "https://www.example.com/healthy-lifestyle-tips",
-      pubDate: "2023-07-26 09:45:00",
-      title: "Tips for a Healthy Lifestyle: Diet and Exercise Recommendations",
-      video_url: null,
-    },
-    // Add more objects as needed...
-  ];
+  const [refreshing, setRefreshing] = React.useState(false);
 
-  console.log(array1[2].title);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setNewState(data.results);
+        setRefreshing(false);
+      })
+      .catch((error) => {
+        alert(error);
+        setRefreshing(false);
+      });
+  });
 
   return (
-    <SafeAreaView>
-      <TouchableOpacity
-        style={{ top: 40, padding: 20, position: "absolute", zIndex: 2 }}
-        onPress={() => navigation.openDrawer()}
+    <SafeAreaView style={{ backgroundColor: "#fff", flex: 1 }}>
+      <ScrollView
+        style={{ paddingBottom: 40 }}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       >
-        <FontAwesome name="navicon" size={30} color="black" />
-      </TouchableOpacity>
-      <ImageBackground
-        source={{
-          uri: "https://wilmotandbennett.net/wp-content/uploads/2023/01/Legal-news-in-brief.jpg",
-        }}
-        style={{
-          width: "100%",
-          height: 240,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View
+        <ImageBackground
+          source={{
+            uri: "https://wilmotandbennett.net/wp-content/uploads/2023/01/Legal-news-in-brief.jpg",
+          }}
           style={{
-            flex: 1,
             width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.6)", // Darken the background color
+            height: 240,
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <Text style={{ fontSize: 30, color: "white", fontWeight: "700" }}>
-            Latest Law News
-          </Text>
-        </View>
-      </ImageBackground>
-      {newState ? (
-        <ScrollView style={{}}>
-          {array1.map(({ title, link, image }) => {
-            return <NewsBox tile={title} link={link} image={image} />;
-          })}
-        </ScrollView>
-      ) : (
-        <View
-          style={{
-            alignContent: "center",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ color: "black", fontSize: 30 }}>An Error occured</Text>
-        </View>
-      )}
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0, 0, 0, 0.6)", // Darken the background color
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ fontSize: 30, color: "white", fontWeight: "700" }}>
+              Latest Law News
+            </Text>
+          </View>
+        </ImageBackground>
+        {newState ? (
+          <View style={{ height: "100%" }}>
+            {newState.map(
+              ({ title, link, image_url, content, pubDate }, key) => {
+                return (
+                  <NewsBox
+                    key={key}
+                    title={title}
+                    link={link}
+                    image={image_url}
+                    content={content}
+                    pubDate={pubDate}
+                  />
+                );
+              }
+            )}
+          </View>
+        ) : (
+          <View
+            style={{
+              alignContent: "center",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "black", fontSize: 30 }}>
+              An Error occured
+            </Text>
+          </View>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- component/NewsBox.js
+++ component/NewsBox.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const NewsBox = ({ link, title, image }) => {
+const NewsBox = ({ link, title, image, content }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <View style={{}}>
-      <Text>{title}Upcoming Movie Releases: Blockbusters to Watch Out For</Text>
-      <Image
-        source={{
-          uri: `{${
-            image ||
-            "https://wilmotandbennett.net/wp-content/uploads/2023/01/Legal-news-in-brief.jpg"
-          }}`,
-        }}
-        style={{ width: 200, height: 200 }}
-      />
+    <View
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: "#ccc",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        justifyContent: "center",
+      }}
+    >
+      <TouchableOpacity
+        style={{ flexDirection: "row", gap: 10 }}
+        onPress={() => setReadMore(!readMore)}
+      >
+        <Image
+          source={{
+            uri:
+              image ||
+              "https://i.pinimg.com/originals/d9/c6/3f/d9c63f660867c4f80a35c437eea48052.png",
+          }}
+          style={{ width: 100, height: 100, flex: 1, borderRadius: 10 }}
+        />
+        <View style={{ flex: 2 }}>
+          <Text style={{ color: "#000" }}>{title}</Text>
+          <Text style={{ color: "blue" }}>{!readMore && "Read more"}</Text>
+        </View>
+      </TouchableOpacity>
+      {readMore && (
+        <Text style={{ paddingTop: 10, color: "#777" }}>{content}</Text>
+      )}
     </View>
   );
 };
